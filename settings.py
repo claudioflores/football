b0699760a396901b1237bd56b8d45e5a710dd4ea
claudioflores <35937594+claudioflores@users.noreply.github.com>
2020-08-05 15:48:08 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'cflores.uc@gmail.com'
+EMAIL_HOST_PASSWORD = 'cayo1987!'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
